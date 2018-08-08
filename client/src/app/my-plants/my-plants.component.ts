b4ed4dbbc00a95bdffa9d0ffcae32b82a357ce7b
Paula <paulagarizcun@gmail.com>
2018-08-08 13:23:27 +0200

    Change notif color

--- client/src/app/my-plants/my-plants.component.ts
+++ client/src/app/my-plants/my-plants.component.ts
@@ -6,6 +6,7 @@ import { notifService } from "../../services/notif.service";
 import { NotificationsService } from "../../../node_modules/angular2-notifications";
 import { environment } from "../../environments/environment";
 import * as moment from 'moment';
+import { ThrowStmt } from "../../../node_modules/@angular/compiler";
 let es = moment.locale("es");
 
 const { BASEURL } = environment;
@@ -51,7 +52,6 @@ export class MyPlantsComponent implements OnInit {
     this.session.isLogged().subscribe(() => {
       this.notifService.getNotif(this.session.user._id).subscribe(data => {
         this.notif = data;
-        console.log(this.notif);
         let content = "";
         this.notif.forEach(e => {
           if (content !== e.message) {
@@ -61,17 +61,12 @@ export class MyPlantsComponent implements OnInit {
             });
 
             toast.click.subscribe(event => {
-              console.log(e)
-              console.log(e.plantId)
               this.plants.forEach(plant => {
                 if(plant._id == e.plantId){
-                  console.log(`Planta antes: ${plant.lastWater}`)
-                  console.log(moment())
                   plant.lastWater = moment().format();
                   let updateLast = plant.lastWater;
                   this.plantsService.updatePlant(plant._id, updateLast).subscribe();
                   this.notifService.removeNotif(e._id).subscribe();
-                  console.log(`Planta despues: ${plant.lastWater}`)
                 }
               })
             });
@@ -96,22 +91,31 @@ export class MyPlantsComponent implements OnInit {
 
   addPlant(plant) {
     //this.plantsService.newPlant(this.newPlant)
-    this.uploader.onBuildItemForm = (item, form) => {
-      form.append("name", plant.name);
-      form.append("birth", plant.birth);
-      form.append("light", plant.light);
-      form.append("room", plant.room);
-      form.append("waterTimes", plant.waterTimes);
-      form.append("lastWater", plant.lastWater);
-      form.append("nextWater", plant.nextWater);
-      form.append("fertilize", plant.fertilize);
-      form.append("transplant", plant.transplant);
-      form.append("author", this.session.user._id);
-    };
-    this.uploader.uploadAll();
-    this.uploader.onCompleteItem = () => {
-      this.refreshPlants();
-    };
+    if((this.uploader._nextIndex == 0) && (this.uploader.queue.length == 0)){
+      this.plantsService.newPlant(plant)
+      .subscribe((plant: any) => {
+        this.newPlant = plant;
+        console.log(plant)
+        this.refreshPlants()
+      })
+    } else {
+      this.uploader.onBuildItemForm = (item, form) => {
+        form.append("name", plant.name);
+        form.append("birth", plant.birth);
+        form.append("light", plant.light);
+        form.append("room", plant.room);
+        form.append("waterTimes", plant.waterTimes);
+        form.append("lastWater", plant.lastWater);
+        form.append("nextWater", plant.nextWater);
+        form.append("fertilize", plant.fertilize);
+        form.append("transplant", plant.transplant);
+        form.append("author", this.session.user._id);
+      };
+      this.uploader.uploadAll();
+      this.uploader.onCompleteItem = () => {
+        this.refreshPlants();
+      };
+    }
   }
 
   refreshPlants() {
