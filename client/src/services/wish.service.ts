aa26f8429d639d786d1e2df158ab52e3b12c742e
Paula <paulagarizcun@gmail.com>
2018-08-09 14:10:21 +0200

    Wish maqueta

--- client/src/services/wish.service.ts
+++ client/src/services/wish.service.ts
@@ -9,20 +9,22 @@ const { BASEURL } = environment;
 export class wishService {
   wish;
 
+  options: object = { withCredentials:true };
+
   constructor(private http: Http) {}
     
   getWishes(author){
-    return this.http.get(`${BASEURL}/api/wishlist/${author}`)
+    return this.http.get(`${BASEURL}/api/wishlist/${author}`, this.options)
     .pipe(map(res => res.json()));
   }
   
   newWish(wish){
-    return this.http.post(`${BASEURL}/api/wishlist/`, wish)
+    return this.http.post(`${BASEURL}/api/wishlist/`, wish, this.options)
     .pipe(map(res => res.json()));
   }
   
   removeWish(id){
-    return this.http.delete(`${BASEURL}/api/wishlist/${id}`)
+    return this.http.delete(`${BASEURL}/api/wishlist/${id}`, this.options)
     .pipe(map(res => res.json()));
   }
 
