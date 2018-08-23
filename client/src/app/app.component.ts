96b7005710a3552539590f0ce3972236e42532f7
Paula <paulagarizcun@gmail.com>
2018-08-23 10:36:39 +0200

    log

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SessionService } from '../services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,10 @@ import { SessionService } from '../services/session.service';
 })
 export class AppComponent {
   title = 'app';
-  constructor(public sessionService:SessionService) { }
+  constructor(public sessionService:SessionService, public router: Router) { }
 
   logout(){
     this.sessionService.logout().subscribe();
+    this.router.navigate(['/home']);
   }
 }
